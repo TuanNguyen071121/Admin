91cc63fd6f95da76d7f9d01c19f1e87fb5f41a58
TuanNguyen071121 <nguyenvantuan071121.34@gmail.com>
2023-06-07 05:35:04 +0700

    0706

--- src/components/adminpage/BanHang/BanHang.jsx
+++ src/components/adminpage/BanHang/BanHang.jsx
@@ -113,15 +113,15 @@ const refresh=async()=>{
                                                                             if (product.id === item.id) {
                                                                             return (
                                                                                 <div className="row mb-4 d-flex justify-content-between align-items-center">
-                                                                    <div className="col-md-2 col-lg-2 col-xl-2">
+                                                                    <div className="col-2">
                                                                         <img src={product.thumbnail}
                                                                             className="img-fluid rounded-3" alt="Cotton T-shirt"/>
                                                                     </div>
-                                                                    <div className="col-md-3 col-lg-3 col-xl-3">
+                                                                    <div className="col">
                                                                         <h6 className="text-muted">Tên sản phẩm</h6>
                                                                         <h6 className="text-black mb-0">{product.name}</h6>
                                                                     </div>
-                                                                    <div className="col-md-3 col-lg-3 col-xl-2 ">
+                                                                    <div className="col-3">
                                                                         <h6 className="text-muted">Số Lượng</h6>
                                                                         <h6 className="text-black mb-0">{item.qty}</h6>
                                                                     </div>
